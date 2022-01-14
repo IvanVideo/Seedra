f0d3e2aea3f5510fb8a21bb2b809101a8bae58b4
IvanVideo <lavochkaentuziastov@yandex.ru>
2022-01-14 16:51:26 +0300

    build: начал отрисовывать страницу блога

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import react from "react";
+import { Routes, Route } from 'react-router-dom';
 import '../../vendor/fonts.css';
 import Header from '../Header/Header';
 import Bunner from '../Bunner/Bunner';
@@ -8,18 +9,24 @@ import Blog from '../Blog/Blog';
 import Slider from '../Slider/Slider';
 import Recommendations from '../Recommendations/Recommendations';
 import Footer from '../Footer/Footer';
+import BlogPage from '../BlogPage/BlogPage';
 
 
 function App() {
   return (
     <div>
       <Header />
-      <Bunner />
-      <Publicity />
-      <Products />
-      <Blog />
-      <Slider />
-      <Recommendations />
+      <Routes>
+        <Route path='/' element={<BlogPage /> }>
+        {/* <Bunner />
+        <Publicity />
+        <Products />
+        <Blog />
+        <Slider />
+        <Recommendations /> */}
+        </Route>
+
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/BlogPage/BlogPage.js
@@ -0,0 +1,16 @@
+import './BlogPage.css';
+
+function BlogPage() {
+    return (
+        <section className='blogPage'>
+            <div className='blogPage__conteiner'>
+                <div className='blogPage__box'>
+                    <h1 className='blogPage__title'>Welcome to our blog</h1>
+                    <p className='blogPage__subtitle'>Here you can find a lot of interesting and useful information that you need in gardening and creating a beautiful garden</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default BlogPage;
